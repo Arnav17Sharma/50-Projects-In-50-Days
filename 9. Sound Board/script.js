ff16558e336065bac62833f01e3ebf4389dc836a
Arnav Sharma <arnavsharma1702@gmail.com>
2020-12-25 17:00:58 +0530

    Added 9. Sound Board(Day 9)!

---
+++ 9. Sound Board/script.js
@@ -0,0 +1,27 @@
+const sounds = ['Band-Kar','Happy-Diwali','Khatam-Tata','Kuch-Bhi','Nahane-Ja']
+
+sounds.forEach(sound => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+    btn.innerText = sound
+
+    btn.addEventListener('click', () => {
+
+        stopSongs()
+
+
+        document.getElementById(sound).play()
+    })
+
+    document.getElementById('buttons').appendChild(btn)
+})
+
+
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound)
+
+        song.pause()
+        song.currentTime = 0;
+    })
+}
